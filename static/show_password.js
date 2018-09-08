7fc2b4102804867fe772cb544577bf76a1708f4d
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2018-09-07 22:04:46 -0300

    Corrected SVG colors, added accordion-like functionality to hamburguer menu and defined behaviors

--- static/show_password.js
+++ static/show_password.js
@@ -4,15 +4,34 @@ $("#show-password").click(function () {
     // now test it's value
     if (type === "password") {
         // change icon
-        $("#show-password-img").hide();
-        $("#hide-password-img").show();
+        $(".show-password-img").hide();
+        $(".hide-password-img").show();
         // toggle field
         $("#password").attr("type", "text");
     } else {
         // change icon
-        $("#hide-password-img").hide();
-        $("#show-password-img").show();
+        $(".hide-password-img").hide();
+        $(".show-password-img").show();
         // toggle field
         $("#password").attr("type", "password");
+    } 
+});
+
+$("#show-password-hamburguer").click(function () {
+    // get the attribute value
+    var type = $("#password-hamburguer").attr("type"); 
+    // now test it's value
+    if (type === "password") {
+        // change icon
+        $(".show-password-img").hide();
+        $(".hide-password-img").show();
+        // toggle field
+        $("#password-hamburguer").attr("type", "text");
+    } else {
+        // change icon
+        $(".hide-password-img").hide();
+        $(".show-password-img").show();
+        // toggle field
+        $("#password-hamburguer").attr("type", "password");
     } 
 });
